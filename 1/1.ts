1e451ab56a456cf1447f2f54b36882ac12df1c8c
Abrorxon-abi <ppvbuyppv@gmail.com>
2025-12-03 10:47:08 +0500

    1 day 2 part

--- 1/1.ts
+++ 1/1.ts
@@ -28,3 +28,32 @@ const part1 = () => {
 };
 
 console.log("part 1:", part1());
+
+const part2 = () => {
+  let counter = 50;
+  let ans = 0;
+
+  for (let i = 0; i < lines.length; i++) {
+    const str = lines[i];
+    const value = Number(str?.slice(1));
+    let full = Math.floor(value / 100);
+    let rest = value % 100;
+    ans += full;
+
+    if (str![0] === "R") {
+      for (let i = 0; i < rest; i++) {
+        counter = (counter + 1) % 100;
+        if (counter === 0) ans++;
+      }
+    } else {
+      for (let i = 0; i < rest; i++) {
+        counter = (counter - 1 + 100) % 100;
+        if (counter === 0) ans++;
+      }
+    }
+  }
+
+  return ans;
+};
+
+console.log("part 2:", part2());
